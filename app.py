a4b507c679f0726d8db6dcb6399223930c5e1018
Luanny Martins <108303052+Luaanny@users.noreply.github.com>
2025-04-28 17:47:32 +0000

    criar_mensagem

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
+
+app = Flask (__name__)
+
+mensagens = []
+
+@app.route('/mensagens', methods=['POST'])
+def criar_mensagens():
+    nova_mensagem = request.get_json()
+    mensagens.append(nova_mensagem)
+    return jsonify ({'mensagem': 'Mensagem criada'}), 201
+
+
+
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
